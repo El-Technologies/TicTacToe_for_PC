dd82ea9a9b59265d4a4ea74638fda3f9a84a850c
Ukaegbe Somtochukwu <99729081+Sohmteee@users.noreply.github.com>
2022-03-01 12:06:31 +0100

    Update data.java

--- TTTGUI/src/data.java
+++ TTTGUI/src/data.java
@@ -5,10 +5,9 @@ public class data {
 
     public static int c = 1; //a counter used to determine the player's turn in this game...you'll see what I mean later
     public static int N = 0; //the user's input
-    public static int n = 0; //random variable
     public static int countDraw = 0; //variable to count the number of turns that have been played without a win. Once it gets to 9 (once every box is filled up), it's considered a draw.
-    public static int randomNumber;
-    public static boolean play = true;
+    public static int randomNumber; //random input for computer
+    public static boolean play = true; //boolean to check if the game is still in session. It's set to false if anyone wins, or it ends in a draw
     public static boolean one = false; //from one through nine, these booleans are used to check if their respective boxes have been chosen previously by a player. In short, it's used to make sure that a box isn't chosen more than once
     public static boolean two = false;
     public static boolean three = false;
@@ -28,8 +27,7 @@ public class data {
     public static JButton Eight;
     public static JButton Nine;
     public static JButton resetButton;
-    public static JLabel info;
-    public static String letter = process.enterLetter();
-
+    public static JLabel info; //a label that shows whose turn it is, who won, when it's a tie, or when an input is invalid
+    public static String letter = process.enterLetter(); //this is an "X" or "O" depending on whose turn it is
 
 }
