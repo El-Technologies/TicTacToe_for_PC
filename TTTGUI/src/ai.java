5dc21723fc2b771521d5ccb487e3e80915fb78a0
Ukaegbe Somtochukwu <99729081+Sohmteee@users.noreply.github.com>
2022-03-08 20:58:45 +0100

    Update ai.java

--- TTTGUI/src/ai.java
+++ TTTGUI/src/ai.java
@@ -9,27 +9,384 @@ public class ai extends data{
         timer.schedule(new TimerTask() { //a timer that delays the computer's input by .5 seconds
             @Override
             public void run() { // algorithms are run to decide the most appropriate input by the computer
-                if (AIPlayO() != -1) N = AIPlayO();  //if the computer sees two Os, it completes it to win
-                else if (AIPlayX() != -1) N = AIPlayX(); //if the computer sees two Xs, it stops you from winning
-                else if ((AIPlayO() == -1) & (AIPlayX() == -1)) N = process.rand();
-                //if neither of the above is noticed, the computer goes ahead to input a random input
-
-                if (process.checkBox()) { //if the input is already occupied...
-                    do { //it continues to input a random input until it plays a valid move
+                switch (level){
+                    case 1 -> {
                         N = process.rand();
-                        if (!process.checkBox()){
-                            break;
-                        }
-                    } while (process.checkBox());
 
+                        if (process.checkBox()) { //if the input is already occupied...
+                            do { //it continues to input a random input until it plays a valid move
+                                N = process.rand();
+                                if (!process.checkBox()){
+                                    break;
+                                }
+                            } while (process.checkBox());
+
+                        }
+
+                        process.display(); //if the input is valid however, it plays it instead
+                    }
+                    case 2 -> {
+                        if (AIPlayO() != -1) N = AIPlayO();  //if the computer sees two Os, it completes it to win
+                        else if (AIPlayX() != -1) N = AIPlayX(); //if the computer sees two Xs, it stops you from winning
+                        else if ((AIPlayO() == -1) & (AIPlayX() == -1)) N = process.rand();
+                        //if neither of the above is noticed, the computer goes ahead to input a random input
+
+                        if (process.checkBox()) {
+                            do {
+                                N = process.rand();
+                                if (!process.checkBox()){
+                                    break;
+                                }
+                            } while (process.checkBox());
+
+                        }
+
+                        process.display();
+                    }
+                    case 3 -> {
+                        if (AIPlayO() != -1) N = AIPlayO();  //if the computer sees two Os, it completes it to win
+                        else if (AIPlayX() != -1) N = AIPlayX(); //if the computer sees two Xs, it stops you from winning
+                        else {
+                            if (rando){
+                                do{
+                                    randomStrategy = process.rand();
+                                    if ((randomStrategy==1) | (randomStrategy==2) | (randomStrategy==3) |
+                                            (randomStrategy==4) | (randomStrategy==5)){
+                                        rando = false;
+                                        break;
+                                    }
+                                } while (rando);
+                            }
+
+                            N = strategy();
+                        }
+
+                        if (N == 0) N = process.rand();
+
+                        if (process.checkBox()) {
+                            do {
+                                N = process.rand();
+                                if (!process.checkBox()){
+                                    break;
+                                }
+                            } while (process.checkBox());
+
+                        }
+
+                        process.display();
+
+                    }
                 }
 
-                process.display(); //if the input is valid however, it plays it instead
 
             }
         }, 500); // delays by .5 seconds before playing its turn
 
     }
+
+    public static int strategy() {
+        if (Five.getText() == "X" & !one & !three & !seven & !nine){
+            do{
+                randomX = process.rand();
+                if ((randomX==1) | (randomX==3) | (randomX==7) |
+                        (randomX==9)){
+                    randx = false;
+                    break;
+                }
+            } while (randx);
+            return randomX;
+        }
+        switch (randomStrategy){
+            case 1 -> {
+                s = strategyOne();
+                if (s != 0) return s;
+            }
+            case 2 -> {
+                s = strategyTwo();
+                if (s != 0) return s;
+            }
+            case 3 -> {
+                s = strategyThree();
+                if (s != 0) return s;
+            }
+            case 4 -> {
+                s = strategyFour();
+                if (s != 0) return s;
+            }
+            case 5 -> {
+                s = strategyFive();
+                if (s != 0) return s;
+            }
+        }
+
+        return 0;
+    }
+
+    public static int strategyOne () {
+        if (!one) return 1;
+        if (One.getText() == "O"){
+
+            if (Five.getText() == "X"){
+                return 9;
+            }
+
+
+            if (Nine.getText() == "O"){
+                if (!four & !seven & !eight){
+                    return 7;
+                }
+                if (!two & !three & !six){
+                    return 3;
+                }
+            }
+
+            if (Two.getText() == "X" & !four & !six &!eight){
+                if (!seven)
+                return 7;
+            }
+            if (Four.getText() == "X" & !two & !six &!eight){
+                if (!three)
+                return 3;
+            }
+            if (Six.getText() == "X" & !four & !two &!eight){
+                if (!nine)
+                return 9;
+            }
+            if (Eight.getText() == "X" & !four & !six &!two){
+                if (!seven)
+                return 7;
+            }
+
+            if (Three.getText() == "X" & !seven & nine){
+                if (!seven)
+                return 7;
+            }
+            if (Seven.getText() == "X" & !three & nine){
+                if (!three)
+                return 3;
+            }
+            if (Nine.getText() == "X" & !seven & three){
+                if (!five)
+                return 5; //it may end in a draw in this case
+            }
+
+            if (Three.getText() == "O" & !five & !seven & !nine){
+                return 5;
+            }
+            if (Seven.getText() == "O" & !five & !three & !nine){
+                return 5;
+            }
+
+            if (Three.getText() == "O" & !five & !six & !nine){
+                return 9;
+            }
+            if (Three.getText() == "O" & !five & !four & !seven){
+                return 9;
+            }
+            if (Seven.getText() == "O" & !five & !eight & !nine){
+                return 9;
+            }
+            if (Seven.getText() == "O" & !five & !two & !three){
+                return 3;
+            }
+
+        }
+        return 0;
+    }
+
+    public static int strategyTwo () {
+        if (!three) return 3;
+        if (Three.getText() == "O"){
+
+            if (Five.getText() == "X"){
+                return 7;
+            }
+
+            if (Seven.getText() == "O"){
+                if (!one & !two & !four){
+                    return 1;
+                }
+                if (!six & !eight & !nine){
+                    return 9;
+                }
+            }
+
+            if (Two.getText() == "X" & !four & !six & !eight){
+                if (!nine)
+                return 9;
+            }
+            if (Four.getText() == "X" & !two & !six & !eight){
+                if (!one)
+                return 1;
+            }
+            if (Six.getText() == "X" & !four & !two & !eight){
+                if (!one)
+                return 1;
+            }
+            if (Eight.getText() == "X" & !four & !six & !two){
+                if (!nine)
+                return 9;
+            }
+
+            if (One.getText() == "X" & !seven & !nine){
+                if (!nine)
+                return 9;
+            }
+            if (Seven.getText() == "X" & !one & !nine){
+                if (!five)
+                return 5; //it'll end in a draw in this case
+            }
+            if (Nine.getText() == "X" & !seven & !one){
+                if (!one)
+                return 1;
+            }
+
+            if (One.getText() == "O" & !five & !seven & !nine){
+                return 5;
+            }
+            if (Nine.getText() == "O" & !five & !one & !seven){
+                return 5;
+            }
+
+            if (Nine.getText() == "O" & !five & !eight & !seven){
+                return 7;
+            }
+            if (Nine.getText() == "O" & !five & !two & !one){
+                return 1;
+            }
+
+        }
+        return 0;
+    }
+
+    public static int strategyThree () {
+        if (!seven) return 7;
+        if (Seven.getText() == "O"){
+
+            if (Five.getText() == "X"){
+                return 3;
+            }
+
+            if (Three.getText() == "O"){
+                if (!one & !two & !four){
+                    return 1;
+                }
+                if (!six & !eight & !nine){
+                    return 9;
+                }
+            }
+
+            if (Two.getText() == "X" & !four & !six & !eight){
+                if (!one)
+                return 1;
+            }
+            if (Four.getText() == "X" & !two & !six & !eight){
+                if (!nine)
+                return 9;
+            }
+            if (Six.getText() == "X" & !four & !two & !eight){
+                if (!nine)
+                return 9;
+            }
+            if (Eight.getText() == "X" & !four & !six & !two){
+                if (!one)
+                return 1;
+            }
+
+            if (One.getText() == "X" & !three & !nine){
+                if (!nine)
+                return 9;
+            }
+            if (Three.getText() == "X" & !one & !nine){
+                if (!five)
+                return 5; //it'll end in a draw in this case
+            }
+            if (Nine.getText() == "X" & !three & !one){
+                if (!one)
+                return 1;
+            }
+
+            if (One.getText() == "O" & !five & !three & !nine){
+                return 5;
+            }
+            if (Nine.getText() == "O" & !five & !three & !one){
+                return 5;
+            }
+
+            if (Nine.getText() == "O" & !five & !eight & !seven){
+                return 7;
+            }
+            if (Nine.getText() == "O" & !five & !two & !one){
+                return 1;
+            }
+
+        }
+        return 0;
+    }
+
+    public static int strategyFour () {
+        if (!nine) return 9;
+        if (Nine.getText() == "O"){
+
+            if (Five.getText() == "X"){
+                return 1;
+            }
+
+            if (One.getText() == "O"){
+                if (!two & !three & !six){
+                    return 3;
+                }
+                if (!four & !seven & !eight){
+                    return 7;
+                }
+            }
+
+            if (Two.getText() == "X" & !four & !six & !eight){
+                if (!three)
+                return 3;
+            }
+            if (Four.getText() == "X" & !two & !six & !eight){
+                if (!seven)
+                return 7;
+            }
+            if (Six.getText() == "X" & !four & !two & !eight){
+                if (!seven)
+                return 7;
+            }
+            if (Eight.getText() == "X" & !four & !six & !two){
+                if (!three)
+                return 3;
+            }
+
+            if (Three.getText() == "O" & !five & !one & !seven){
+                return 5;
+            }
+            if (Seven.getText() == "O" & !five & !three & !one){
+                return 5;
+            }
+
+            if (Three.getText() == "O" & !five & !two & !one){
+                return 1;
+            }
+            if (Three.getText() == "O" & !five & !seven & !eight){
+                return 7;
+            }
+            if (Seven.getText() == "O" & !five & !one & !four){
+                return 1;
+            }
+            if (Seven.getText() == "O" & !five & !three & !six){
+                return 3;
+            }
+
+        }
+        return 0;
+    }
+
+    public static int strategyFive () {
+        if (!five) return 5;
+        return 0;
+    }
+
 
     public static int AIPlayO () {
         /*this method calls on the AICheckTwoOs to check if there are two Os on the board
