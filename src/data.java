c03c4f50a7e7dd5efeeda6b17e63007ed2e1c631
Ukaegbe Somtochukwu <99729081+Sohmteee@users.noreply.github.com>
2022-03-25 08:52:10 +0100

    Add files via upload

---
+++ src/data.java
@@ -0,0 +1,15 @@
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+public class data {
+
+    public static int c = 1, C = 1, N = 0, countDraw = 0, randomNumber, randomStrategy, randomX, randoX, level = 0,
+            x, o, s;
+    public static boolean play = true, one = false, two = false, three = false, four = false, five = false,
+            six = false, seven = false, eight = false, nine = false;
+    public static boolean Player_vs_Player = true, rando = true, randx = true, ranx = true;
+    public static JButton One, Two, Three, Four, Five, Six, Seven, Eight, Nine,
+            resetButton, menuButton, PVPBtn, PVCBtn, EasyBtn, MediumBtn, HardBtn;
+    public static JLabel info, XLabel, OLabel, XScore, OScore;
+    public static String letter;
+}
